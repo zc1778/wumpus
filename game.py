e2d9adae3640b1ca6d3a0d3c3692afff4bcf434e
zc1778 <zcraddock1778@gmail.com>
2025-05-08 19:18:09 -0400

    added create_cave function with pre-defined layout

--- game.py
+++ game.py
@@ -5,3 +5,18 @@ class Room:
         self.pit = pit
         self.left = None
         self.right = None
+        self.below = None
+
+# create pre-defined cave and return entrance room/node
+def create_cave():
+    node = Room(False, False, False)
+    node.right = Room(False, False, False)
+    node.left = Room(False, False, False)
+    node.right.below = node
+    node.left.below = node
+    node = node.left
+    node.right = Room(False, True, False)
+    node.left = Room(True, False, False)
+    node = node.below
+    node.right.right = Room(False, False, True)
+    return node
